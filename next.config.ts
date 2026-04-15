0da9df6a8c5ed4c8bef26c573cb21db2f28a8b70
RiyazurRahman <riyazurrahman@RiyazurRahmans-MacBook-Pro.local>
2026-04-15 17:55:00 +0400

    fix: disable image optimization to fix logo on local network

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.39.1.55', '10.39.8.59', 'alwahaapools.local'],
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     proxyClientMaxBodySize: '25mb',
   },
